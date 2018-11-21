24f31dfa95bc6d80ac927d96bcf2e13bf6956a2a
Alejandro Esquivel <yoquiale@gmail.com>
2018-11-22 00:43:37 +0100

    edited js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-var links = document.querySelectorAll('.navbar > ul > li > a');
-for(var link of links){
-  console.log(link);
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log(e);
-  });
-}
-
 //Redirigir svg usuario a página de login/registro al hacer click
 document.querySelector('li.user > svg').addEventListener('click', function(){
   document.location= 'log.html';

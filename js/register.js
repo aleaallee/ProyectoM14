127ccfb587216302094b9809110b6afcf8f12d52
Alejandro Esquivel <yoquiale@gmail.com>
2018-11-21 16:18:07 +0100

    added js

--- js/register.js
+++ js/register.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-    $('.user').click(function(e){
-        console.log(e);
-    })
+$(document).on("submit", "form.regform", function(event){
+    event.preventDefault();
+    //console.log(event);
+    var form = $(this);
+    //Datos de los inputs del formulario
+    var datos = {
+        user : $("input[name='user']", form).val(),
+        email : $("input[name='email']", form).val(),
+        password : $("input[name='password']", form).val(),
+        reppassword : $("input[name='reppassword']", form).val()
+    }
+    console.table(datos);
 });
